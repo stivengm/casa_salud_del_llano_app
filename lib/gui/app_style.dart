97cb21892d4bb3e4e4bf8e1c75c7cfe6c9315879
Stiven Gomez <stivengomez@soyyo.co>
2023-08-09 18:58:35 -0500

    ADD - ADD Styles App.

---
+++ lib/gui/app_style.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppStyle {
+
+  static const Color primaryColor = Color(0xFF0C9869);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color blackColor = Color(0xFF000000);
+
+  static const Color kTextColor = Color(0xFF3C4046);
+
+}
