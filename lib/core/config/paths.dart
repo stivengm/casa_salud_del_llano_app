e849c4a28099d8a38d87161887be8f06b0e8b1fd
Stiven Gomez <stivengomez@soyyo.co>
2023-08-09 20:41:48 -0500

    FIX - Add newStore.

--- lib/core/config/paths.dart
+++ lib/core/config/paths.dart
@@ -1,7 +1,8 @@
-import 'package:casa_salud_del_llano_app/gui/views/admin/register_view.dart';
-import 'package:casa_salud_del_llano_app/gui/views/stores_view/stores_view.dart';
 import 'package:flutter/material.dart';
 
+import 'package:casa_salud_del_llano_app/gui/views/admin/register_view.dart';
+import 'package:casa_salud_del_llano_app/gui/views/stores_view/new_store_view.dart';
+import 'package:casa_salud_del_llano_app/gui/views/stores_view/stores_view.dart';
 import 'package:casa_salud_del_llano_app/gui/views/admin/login_view.dart';
 import 'package:casa_salud_del_llano_app/gui/views/dashboard_view.dart';
 
@@ -11,5 +12,6 @@ Map<String, WidgetBuilder> routesApp() => <String, WidgetBuilder> {
   'login': ( _ ) => const LoginView(),
   'register': ( _ ) => const RegisterView(),
   'stores': ( _ ) => const StoresView(),
+  'newStore': ( _ ) => const NewStoreView(),
 
 };
