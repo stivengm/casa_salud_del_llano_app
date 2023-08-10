d53490cb9430a04c78a26693aa57558471c8e5ad
Stiven Gomez <stivengomez@soyyo.co>
2023-08-09 20:02:08 -0500

    FIX - Fix button disabled.

--- lib/gui/app_style.dart
+++ lib/gui/app_style.dart
@@ -5,6 +5,7 @@ class AppStyle {
   static const Color primaryColor = Color(0xFF0C9869);
   static const Color whiteColor = Color(0xFFFFFFFF);
   static const Color blackColor = Color(0xFF000000);
+  static const Color greyColor = Colors.grey;
 
   static const Color kTextColor = Color(0xFF3C4046);
 
