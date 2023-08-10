7f4895169532de2a0d8b703728b1c6e9a67bfbba
Stiven Gomez <stivengomez@soyyo.co>
2023-08-09 20:39:13 -0500

    FIX - Add storesView.

--- lib/core/config/paths.dart
+++ lib/core/config/paths.dart
@@ -1,4 +1,5 @@
 import 'package:casa_salud_del_llano_app/gui/views/admin/register_view.dart';
+import 'package:casa_salud_del_llano_app/gui/views/stores_view/stores_view.dart';
 import 'package:flutter/material.dart';
 
 import 'package:casa_salud_del_llano_app/gui/views/admin/login_view.dart';
@@ -9,5 +10,6 @@ Map<String, WidgetBuilder> routesApp() => <String, WidgetBuilder> {
   'dashboard': ( _ ) => const DashboardView(),
   'login': ( _ ) => const LoginView(),
   'register': ( _ ) => const RegisterView(),
+  'stores': ( _ ) => const StoresView(),
 
 };
