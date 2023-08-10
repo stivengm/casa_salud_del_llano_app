4194e60863a2eefeaec6012d3f6bf999d9443092
Stiven Gomez <stivengomez@soyyo.co>
2023-08-09 20:38:42 -0500

    ADD - Add StoresView.

---
+++ lib/gui/views/stores_view/stores_view.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class StoresView extends StatelessWidget {
+  const StoresView({ Key? key }) : super( key: key );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Droguerías"),
+      ),
+      body: _body(),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(
+          Icons.add
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget _body() {
+    return ListView(
+      children: [
+        ListTile(
+          contentPadding: const EdgeInsets.only(left: 23.0, right: 20.0),
+          title: const Text("Aguazul Casanare"),
+          subtitle: const Text("Carrera 4"),
+          trailing: const Icon(
+            Icons.chevron_right_rounded,
+            size: 30.0,
+          ),
+          onTap: () {},
+        ),
+        ListTile(
+          contentPadding: const EdgeInsets.only(left: 23.0, right: 20.0),
+          title: const Text("Pararito"),
+          subtitle: const Text("Carrera 4"),
+          trailing: const Icon(
+            Icons.chevron_right_rounded,
+            size: 30.0,
+          ),
+          onTap: () {},
+        ),
+        ListTile(
+          contentPadding: const EdgeInsets.only(left: 23.0, right: 20.0),
+          title: const Text("Yopal"),
+          subtitle: const Text("Carrera 4"),
+          trailing: const Icon(
+            Icons.chevron_right_rounded,
+            size: 30.0,
+          ),
+          onTap: () {},
+        ),
+      ],
+    );
+  }
+}
